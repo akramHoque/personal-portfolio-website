2b31296322b8d573d16594e809d868dec325d2e2
unknown <oriniusakram511@gmail.com>
2022-06-11 10:44:46 +0600

    added portfolio

--- src/Components/Portfolio/Portfolio.js
+++ src/Components/Portfolio/Portfolio.js
@@ -1,8 +1,49 @@
 import React from 'react';
-
+import './Portfolio.css'
+import img1 from '../../Assests/portfolio1.png'
+import img2 from '../../Assests/portfolio2.png'
+import img3 from '../../Assests/portfolio3.png'
 const Portfolio = () => {
     return (
-       <section id='portfolio'>Portfolio</section>
+        <section id='portfolio'>
+            <h5>My Recent Work</h5>
+            <h2>Portfolio</h2>
+            <div className="container portfolio__container">
+                <article className='portfolio__item'>
+                    <div className="portfolio__image-item">
+                        <img src={img3} alt="" />
+                    </div>
+                    <h3> Organic Fruits Warehouse</h3>
+                    <div className='portfolio__item-cta'>
+                        <a href="https://github.com/akramHoque/manufacturer-website-client-side" className='btn'>Client Side</a>
+                        <a href="https://github.com/akramHoque/manufacturer-website-server" className='btn'>Server Side</a>
+                        <a href="https://assignment-12-53a87.web.app/" className='btn'>Live Site</a>
+                    </div>
+                </article>
+                <article className='portfolio__item'>
+                    <div className="portfolio__image-item">
+                        <img src={img2} alt="" />
+                    </div>
+                    <h3>  Gym Trainer Website(Independent Service Provider)</h3>
+                    <div className='portfolio__item-cta'>
+                        <a href="https://github.com/akramHoque/manufacturer-website-client-side" className='btn'>Client Side</a>
+                        <a href="https://github.com/akramHoque/manufacturer-website-server" className='btn'>Server Side</a>
+                    </div>
+                </article>
+                <article className='portfolio__item'>
+                    <div className="portfolio__image-item">
+                        <img src={img1} alt="" />
+                    </div>
+                    <h3> Zenith Manufacturing Company</h3>
+                    <div className='portfolio__item-cta'>
+                        <a href="https://github.com/akramHoque/manufacturer-website-client-side" className='btn'>Client Side</a>
+                        <a href="https://github.com/akramHoque/manufacturer-website-server" className='btn'>Server Side</a>
+                        <a href="https://assignment-12-53a87.web.app/" className='btn'>Live Site</a>
+                    </div>
+
+                </article>
+            </div>
+        </section>
     );
 };
 
